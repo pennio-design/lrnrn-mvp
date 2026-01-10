233bf3393bcf0a899e65f8e0f31672bca5dd2919
PAUL ADEBISI OYATOWO <106627306+pennio-design@users.noreply.github.com>
2026-01-10 22:31:39 +0100

    Add base path for deployment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
+    base: '/lrnrn-mvp/', // Add this line right here
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
